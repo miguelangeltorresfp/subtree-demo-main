85f283f2afe20a01c4ee12baaee7e1ed2d472f95
miguelangeltorresfp <miguelangeltorresfp@gmail.com>
2018-09-10 21:40:58 +0200

    arbitrary change to subtree-demo-plugin on copia

--- subtree-demo-plugin/lib/index.js
+++ subtree-demo-plugin/lib/index.js
@@ -6,6 +6,7 @@
   function DemoPlugin() {
     console.log("Yay plugin!");
     console.log("modificacion arbitraria");
+    console.log("modificacion arbitraria 2")
   }
 
   plugins.register(DemoPlugin);
